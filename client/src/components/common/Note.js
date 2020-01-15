4d3f90c13c345c0d08ac273c43bb92ee502d12a9
cjquines <cjquines@yahoo.com>
2020-01-14 19:12:19 -0500

    add Note object

---
+++ client/src/components/common/Note.js
@@ -0,0 +1,15 @@
+/**
+ * A note in a song.
+ * @param {number} pitch - from 0 to 127, correspond to C-1 to G9, according to MIDI specification
+ * @param {number} onset - in ms, amount of time after start that the note is played
+ * @param {number} length - in ms, amount of time the note is played
+ */
+class Note {
+  constructor(pitch, onset, length) {
+    this.pitch = pitch;
+    this.onset = onset;
+    this.length = length;
+  }
+}
+
+export default Note;
