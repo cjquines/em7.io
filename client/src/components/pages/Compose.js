63beb2a82b5b0715f8f8940f227b2cdaeb4b600f
cjquines <cjquines@yahoo.com>
2020-01-15 23:19:40 -0500

    add SongParameterInput

--- client/src/components/pages/Compose.js
+++ client/src/components/pages/Compose.js
@@ -5,6 +5,7 @@ const Soundfont = require("soundfont-player");
 
 import Note from "../common/Note.js";
 import Song from "../common/Song.js";
+import SongParameterInput from "../modules/SongParameterInput.js";
 
 import "../../utilities.css";
 
@@ -27,6 +28,7 @@ class Compose extends Component {
     this.pressKey = this.pressKey.bind(this);
     this.releaseKey = this.releaseKey.bind(this);
     this.noteBlock = this.noteBlock.bind(this); // TODO: factor out
+    this.record = this.record.bind(this);
   }
 
   pressKey(key, pitch) {
@@ -76,11 +78,38 @@ class Compose extends Component {
     ));
   }
 
+  record() {
+
+  }
+
   render() {
     return (
       <div className="Compose-container">
       compose page.
+
+      <SongParameterInput
+        song={this.state.song}
+        parameter="key"
+        text="Key"
+        defaultValue="C"
+        onChange={(song) => this.setState({song: song})}
+      />
+      <SongParameterInput
+        song={this.state.song}
+        parameter="signature"
+        text="Time Signature"
+        defaultValue="4/4"
+        onChange={(song) => this.setState({song: song})}
+      />
+      <SongParameterInput
+        song={this.state.song}
+        parameter="tempo"
+        text="tempo"
+        defaultValue="120"
+        onChange={(song) => this.setState({song: song})}
+      />
       
+      <button type="button" onClick={this.record}>Record</button>
       {this.noteBlock()}
       </div>
     );
