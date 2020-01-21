d9bd1e0865d96a9d8d85d9f2bdaa4cda0bc2b7ad
Dylan Liu <dylanliu@mit.edu>
2020-01-20 22:58:20 -0500

    get song api

--- server/api.js
+++ server/api.js
@@ -33,6 +33,10 @@ router.get("/whoami", (req, res) => {
 // | write your API methods below!|
 // |------------------------------|
 
+router.get("/song", (req, res) => {
+  song.find({user : req.query._id}).then((songs) => res.send(songs));
+});
+
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
   console.log(`API route not found: ${req.method} ${req.url}`);
