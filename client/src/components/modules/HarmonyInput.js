c99ae05d43fa100a74e8d85f0cf9d85baa7c538c
Dylan Liu <dylanliu@mit.edu>
2020-01-20 22:27:33 -0500

    harmonyinput upload

---
+++ client/src/components/modules/HarmonyInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+/**
+ * Input panel that changes the song tempo.
+ *
+ * Proptypes
+ * @param {Song} song: the song that we're changing parameters of
+ * @param {number} snapInterval: the snap interval
+ * @param {number} defaultTempo: default tempo
+ * @param {(Song) => void} onChange: (function) triggered when changing song parameter
+ */
+class HarmonyInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      harmonyOption : this.props.harmonyOption,
+      harmonyChords : this.props.harmonyChords,
+    };
+  }
+
+  handleHarmonyChange = (event) => {
+    this.setState({harmonyOption: event.target.value}),
+    this.props.onChange(event.target.value);
+  };
+
+  render() {
+    return (
+      <div className="TempoInput-container">
+        Amount of Harmony Options: {this.props.harmonyChords.length}
+        <input value={this.props.harmonyOption} type="number" min="1" max= {this.props.harmonyChords.length} onChange={this.handleHarmonyChange}/>
+      </div>
+    );
+  }
+}
+
+export default HarmonyInput;
